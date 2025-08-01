e7ab4dfcd59daa47c5d57c75488481cad2ba4236
Redfreet <ravigood7@gmail.com>
2025-08-02 01:30:17 +0530

    secture change

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -139,7 +139,7 @@ export const login = async (req, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "none",
-        secure: process.env.NODE_ENV !== "development",
+        secure: true,
       })
       .json({
         _id: user._id,
