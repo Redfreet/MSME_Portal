90905d6d8c385638c8db52a24f66d6c7a281a664
Ravi <155631417+Redfreet@users.noreply.github.com>
2025-08-02 01:15:48 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 const corsOptions = {
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
   origin: "https://msmeportal.vercel.app",
   credentials: true,
 };
